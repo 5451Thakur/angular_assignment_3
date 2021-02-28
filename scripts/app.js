4afd723a5a51b7096c3c18cf02cf11ca90217b52
5451Thakur <shashi@bcs.org>
2021-02-28 14:00:40 +0100

    Ready for assessment

---
+++ scripts/app.js
@@ -0,0 +1,83 @@
+(function() {
+  'use strict';
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItems)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/menu_items.json')
+
+  NarrowItDownController.$inject =['MenuSearchService']
+  function NarrowItDownController(MenuSearchService){
+    var nid = this;
+    var items = [];
+    // nid.message = "";
+    // nid.searchTerm = "";
+
+    nid.search = function(){
+      if(nid.searchTerm.length != 0){
+        var promise = MenuSearchService.getMatchedMenuItems(nid.searchTerm);
+        nid.found = "";
+        promise.then(function(response){
+          var num_items_found = response.data.length;
+          if( num_items_found > 0){
+            nid.message = ("Found " + num_items_found + " matching items for " + nid.searchTerm);
+            nid.found = response.data;
+          }else { nid.message = "Nothing found"; } //No matching items were found
+        }).catch(function(error){
+          nid.message = "Nothing found"; //If error is caught in the promise
+        })
+      }
+      else{ nid.message = "Nothing found";}  //No search term provided
+    };
+
+    nid.remove = function (itemIndex) {
+      console.log("I got: " + itemIndex);
+    nid.found.splice(itemIndex, 1);
+  };
+
+  }
+
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath']
+  function MenuSearchService($http, ApiBasePath){
+    var service = this;
+
+      service.getMatchedMenuItems = function(term){
+        var items = [];
+
+        var r = $http({
+            method: "GET",
+            url: ApiBasePath
+          });
+          r.then(function(response){
+            var i = 0;
+            for(i=0; i< response.data['menu_items'].length; i++ ){
+              var d = response.data['menu_items'][i]["description"];
+              if(d.toLowerCase().indexOf(term.toLowerCase()) !== -1){
+                items.push({name: response.data['menu_items'][i]["name"],
+                description: response.data['menu_items'][i]["description"],
+                short_name: response.data['menu_items'][i]["short_name"]});
+                }
+              }
+              response.data = items;
+          }, function(response){ });
+          return r;
+        }
+
+  }
+
+
+  function FoundItems(){
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope:{
+        items: '<',
+        remove: '&'
+      }
+    };
+    return ddo;
+
+  }
+
+
+}());
